fc32ccb52aeb0cd4f31bc8101014eeaa60733a29
Sergio Yonashiro <sergio.yonashiro@outlook.com>
2019-10-22 20:20:54 -0300

    Updates

---
+++ precache-manifest.61b66a67550b0a462058c9bc5cbbd15e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a01f004a04c24f2ab49",
+    "url": "/CartolaFC/static/css/main.b760bafb.chunk.css"
+  },
+  {
+    "revision": "5a01f004a04c24f2ab49",
+    "url": "/CartolaFC/static/js/main.5a01f004.chunk.js"
+  },
+  {
+    "revision": "7ad24eceb1085e230936",
+    "url": "/CartolaFC/static/js/1.7ad24ece.chunk.js"
+  },
+  {
+    "revision": "6b89d4e7ab7b2b82fa07",
+    "url": "/CartolaFC/static/js/runtime~main.6b89d4e7.js"
+  },
+  {
+    "revision": "cad566ce91d44b358db860940e44072a",
+    "url": "/CartolaFC/index.html"
+  }
+];
